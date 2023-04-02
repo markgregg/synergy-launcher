29ba1a417ecd959c43897fe748cb8cffa9d6073e
markgregg <gregg.mark@gmail.com>
2023-04-02 15:04:03 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useState, KeyboardEvent, ChangeEvent } from 'react';
 import './App.css';
 
-
 const App = () => {
+  const [inputText,setInputText] = useState<string>("");
+
+  const textChanged = (event: ChangeEvent<HTMLInputElement>) => {
+
+    setInputText(event.target.value);
+  }
+
+  const keyPressed = (event: KeyboardEvent<HTMLInputElement>) => {
+
+  }
+
   return (
-    <div className='Launcher'>
+    <div className="launcher">
+      <div className="titleBar">
+        <div className="titleText">Synergy</div>
+      </div>
+      <div className="searchContainer">
+        <input 
+          className="search"
+          type="text" 
+          placeholder="Type to search"
+          value={inputText}
+          spellCheck="false"
+          autoCapitalize="off"
+          autoComplete="off"
+          autoCorrect="off"
+          onChange={textChanged}
+          onKeyDownCapture={keyPressed}
+        />
+      </div>
     </div>
   );
 }
